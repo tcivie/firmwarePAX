ed60052db9c46a3c7a990ccada1dc47405c21365
Gleb Tcivie <gleb@tcivie.com>
2025-04-13 22:57:45 +0300

    Fix generation script

--- bin/regen-protos.sh
+++ bin/regen-protos.sh
@@ -7,5 +7,5 @@ echo "firmware root directory if the following step fails, you should download t
 echo "prebuilt binaries for your computer into nanopb-0.4.9"
 
 # the nanopb tool seems to require that the .options file be in the current directory!
-cd protobufs
-../nanopb-0.4.9/generator-bin/protoc --experimental_allow_proto3_optional "--nanopb_out=-S.cpp -v:../src/mesh/generated/" -I=../protobufs meshtastic/*.proto
+cd ../protobufs
+protoc --experimental_allow_proto3_optional "--nanopb_out=-S.cpp -v:../src/mesh/generated/" -I=../protobufs meshtastic/*.proto
